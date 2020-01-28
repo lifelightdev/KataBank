9b5c3c015979d0c53810f7e69d86a7b539c7f698
Life Light <lifelightdev@gmail.com>
2020-01-28 18:15:43 +0100

    refactor calculate balance in account with a for

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -5,24 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
-
-    private int balance = 0;
     private List<Transaction> transactions = new ArrayList<>();
 
     public int getBalance() {
+        int balance = 0;
+        for (Transaction transaction: transactions){
+            balance = transaction.calculateBalance(balance);
+        }
         return balance;
     }
 
     public void deposit(LocalDate date, int amount) {
         Deposit deposit = new Deposit(date,amount);
         transactions.add(deposit);
-        balance = deposit.calculateBalance(balance);
     }
 
     public void withdrawal(LocalDate date, int amount) {
         Withdrawal withdrawal = new Withdrawal(date,amount);
         transactions.add(withdrawal);
-        balance = withdrawal.calculateBalance(balance);
     }
 
     public String printHistoryOperations() {
