784792b3cc61e34c95039e0537c87bcbf36965d1
Life Light <lifelightdev@gmail.com>
2020-01-28 11:15:07 +0100

    refactor test with setup

--- src/test/java/bank/AccountTest.java
+++ src/test/java/bank/AccountTest.java
@@ -1,29 +1,26 @@
 package bank;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
+    private Account account;
+
+    @BeforeEach
+    private void setup(){
+         account = new Account();
+    }
+
     @Test
     void Shouls_balanceReturn0_When_noTransaction() {
-
-        // Give
-        Account account = new Account();
-
-        // When
-
-        //Then
         assertThat(account.getBalance()).isEqualTo(0);
-
     }
 
     @Test
     void Should_balanceReturn1_When_firstDeposit1() {
-
-        // Give
-        Account account = new Account();
 
         // When
         account.deposit(1);
