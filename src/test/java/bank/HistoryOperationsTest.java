9c84f3896700d93dfa58f817a2c8b3ebbf899add
Life Light <lifelightdev@gmail.com>
2020-01-28 14:52:54 +0100

    refactor HistoryOperationsTest

--- src/test/java/bank/HistoryOperationsTest.java
+++ src/test/java/bank/HistoryOperationsTest.java
@@ -1,5 +1,6 @@
 package bank;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -10,6 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HistoryOperationsTest {
 
+    private List<Transaction> transactions;
+
     private final static String LINE_SEPARATOR = System.getProperty("line.separator");
 
     private final static LocalDate THE_2020_01_01 = LocalDate.of(2020,1,1);
@@ -17,10 +20,13 @@ public class HistoryOperationsTest {
     private final static int AMOUNT_10 = 10;
     private final static int AMOUNT_20 = 20;
 
+    @BeforeEach
+    private void setup(){
+        transactions = new ArrayList<>();
+    }
+
     @Test
     void Should_returnHistoryOfOneOperation_When_deposit(){
-        // Give
-        List<Transaction> transactions = new ArrayList<>();
         // When
         transactions.add(new Deposit(THE_2020_01_01, AMOUNT_10));
         // Then
@@ -29,8 +35,6 @@ public class HistoryOperationsTest {
 
     @Test
     void Should_returnHistoryOfSomeOperationDeposit_When_someDeposit(){
-        // Give
-        List<Transaction> transactions = new ArrayList<>();
         // When
         transactions.add(new Deposit(THE_2020_01_01, AMOUNT_10));
         transactions.add(new Deposit(THE_2020_01_02, AMOUNT_20));
@@ -38,4 +42,5 @@ public class HistoryOperationsTest {
         assertThat(Print.historyOperations(transactions)).isEqualTo("Deposit | 2020-01-01 | 10 | 10" + LINE_SEPARATOR +
                 "Deposit | 2020-01-02 | 20 | 30" + LINE_SEPARATOR);
     }
+
 }
